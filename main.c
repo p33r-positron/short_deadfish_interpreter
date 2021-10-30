fd2c402c998b708954c364a17eeb6dd3096cd875
p33r-positron <84068153+p33r-positron@users.noreply.github.com>
2021-10-30 14:06:35 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,2 @@
+#include <stdio.h>
+int main(){int x=0,i;a:i=getchar();if(x>255||x<0)x=0;if(i=='i')++x;else if(i=='d')--x;else if(i=='o')printf("%d\n",x);else if(i=='s')x*=x;else putchar(10);goto a;}
